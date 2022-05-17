14a9fc51fb5a8630ae1604ac13c298617eb76b0f
cesar <cesarluilly.garcia@hotmail.com>
2022-05-16 22:44:33 -0500

    refactoring

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ const Reader = require("./lib/utils/Reader")
 const ExplorerService = require("./lib/services/ExplorerService")
 
 const explorers = Reader.readJsonFile("explorers.json")
-console.log(explorers)
+//  //console.log(explorers)
 
 //  //Aplicacion de ExplorerService sobre la lista de explorer
 
 console.log(ExplorerService.filterByMission(explorers, "node"));
 
+// Aplicación del ExplorerService sobre la lista de explorers
+ExplorerService.filterByMission(explorers, "node");
+ExplorerService.getAmountOfExplorersByMission(explorers, "node");
+ExplorerService.getExplorersUsernamesByMission(explorers, "node");
--- readme.md
+++ readme.md
@@ -1,11 +1,11 @@
-# Instrucciones
+# Como se inicio este proyecto
 
-1.- Instalamos dependencias con npm install
+1.- npm init -y
 
-2.- Ejecutamos el server con node app.js
+2.- npm install --save-dev jest
 
-# Herramientas
+Herramientas
 
 Basado en las instrucciones de la siguiente URL de programa de capacitacion por LaunchX
 
-[express_basic](https://github.com/LaunchX-InnovaccionVirtual/MissionNodeJS/blob/main/semanas/semana_3/6_express_server.md)
+[refactoring](https://github.com/LaunchX-InnovaccionVirtual/MissionNodeJS/blob/main/semanas/semana_4/1_refactoring_fizzbuzz_parte1.md)
